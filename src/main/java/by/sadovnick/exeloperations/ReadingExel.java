eacb8cbfe78688305a2d68fac19ad17841e05426
Sadonikov Alexander <alecos100@gmail.com>
2023-06-24 13:24:07 +0300

    Read data from exel

---
+++ src/main/java/by/sadovnick/exeloperations/ReadingExel.java
@@ -0,0 +1,59 @@
+package by.sadovnick.exeloperations;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ReadingExel {
+    public static void main(String[] args) throws IOException {
+        String exelFilePath = ".\\datafiles\\countries.xlsx";
+        FileInputStream inputStream = new FileInputStream(exelFilePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);//        workbook.getSheet("Лист1");
+
+        //USING FOR LOOP
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(1).getLastCellNum();
+        for (int r = 0; r <= rows; r++) {
+            XSSFRow row = sheet.getRow(r);
+            for (int c = 0; c < cols; c++) {
+                XSSFCell cell = row.getCell(c);
+                parseCellType(cell);
+            }
+            System.out.println();
+        }
+
+        //USING ITERATOR
+        for (Row cells : sheet) {
+            XSSFRow row = (XSSFRow) cells;
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                XSSFCell cell = (XSSFCell) cellIterator.next();
+                parseCellType(cell);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void parseCellType(XSSFCell cell) {
+        switch (cell.getCellType()) {
+            case STRING:
+                System.out.print(cell.getStringCellValue());
+                break;
+            case NUMERIC:
+                System.out.print(cell.getNumericCellValue());
+                break;
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+        }
+        System.out.print(" | ");
+    }
+}
